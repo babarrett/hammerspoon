7b721104275f25d05bee15b18199cc1e0ff8bd3a
Bruce Barrett <bbarrett@asperasoft.com>
2016-11-17 15:19:39 -0800

    Move "pastecurrentsafariurl" out of init and into its own file

--- init.lua
+++ init.lua
@@ -9,20 +9,3 @@ hs.hotkey.bind(HyperFn, "W", function()
  hs.alert.show("Hello World!")
 end)
 
--- Found at: https://github.com/cmsj/hammerspoon-config/blob/master/init.lua
--- Rather than switch to Safari, copy the current URL, switch back to the previous app and paste,
--- This is a function that fetches the current URL from Safari and types it
-function typeCurrentSafariURL()
-    script = [[
-    tell application "Safari"
-        set currentURL to URL of document 1
-    end tell
-    return currentURL
-    ]]
-    ok, result = hs.applescript(script)
-    if (ok) then
-        hs.eventtap.keyStrokes(result)
-    end
-end
-
-hs.hotkey.bind(HyperFn, 'u', typeCurrentSafariURL)
