005fe98e351350e508451aa290ea504c5d7cacb1
BruceBarrett <bruceb@earthreflections.com>
2016-11-17 16:16:46 -0800

    Update internal docs

--- init.lua
+++ init.lua
@@ -2,10 +2,9 @@
 --	Cycle through displays: http://bezhermoso.github.io/2016/01/20/making-perfect-ramen-lua-os-x-automation-with-hammerspoon/
 --	for apps:	hs.hotkey.bind(HyperFn, 'D', function () hs.application.launchOrFocus("Dictionary") end)
 
---	Hellow world mapped to Hyper=W
-HyperFn = {"cmd", "alt", "ctrl", "shift"}
+HyperFn = {"cmd", "alt", "ctrl", "shift"}	-- Mash the 4 modifier keys for some new function
 
+--	Hello world mapped to HyperFn+W
 hs.hotkey.bind(HyperFn, "W", function()
- hs.alert.show("Hello World!")
+  hs.alert.show("Hello World!")
 end)
-
