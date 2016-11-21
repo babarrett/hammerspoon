71fa4d4007b9501340baa3d34dcdbd9bca380823
BruceBarrett <bruceb@earthreflections.com>
2016-11-20 20:44:15 -0800

    Add pasteCurrentSafariUrlX.lua back in. CamelCase

---
+++ pasteCurrentSafariUrlX.lua
@@ -0,0 +1,39 @@
+local pasteCurrentSafariUrl = {}
+
+-- Found at: https://github.com/cmsj/hammerspoon-config/blob/master/init.lua
+-- Modified to fit my preferred method of adding functions
+
+-- Rather than switch to Safari, copy the current URL, switch back to the previous app and paste,
+-- this is a function that fetches the current URL from Safari and types it into the current app.
+
+-- private functions to be referenced & executed later.
+function typeCurrentSafariURL()
+    script = [[
+    tell application "Safari"
+        set currentURL to URL of document 1
+    end tell
+    return currentURL
+    ]]
+    ok, result = hs.applescript(script)
+    if (ok) then
+        hs.eventtap.keyStrokes(result)
+    end
+end
+
+-- private
+local helpString = ""
+local funNameToFunction = {
+	pasteSafariUrl = typeCurrentSafariURL
+}
+
+function pasteCurrentSafariUrl.bind(modifiers, char, functName)
+	-- Bind it to HyperFn+U
+	helpString = "Hyper+" .. char .. "     - Fetch the current URL from Safari and type it.\n"
+	hs.hotkey.bind(modifiers, char, funNameToFunction[functName] )
+end
+
+function pasteCurrentSafariUrl.getHelpString()
+	return helpString
+end
+
+return pasteCurrentSafariUrl
