ea4e5fae75b0506961ab7a6ad148c19ba31e2656
Bruce Barrett <bbarrett@asperasoft.com>
2016-12-07 17:49:21 -0800

    Added support for HyperFn+= Show mouse cursor

--- miscFunctions.lua
+++ miscFunctions.lua
@@ -68,4 +68,41 @@ function miscFunctions.bind(modifiers, char, functName)
 	HF.add("Hyper+" .. char .. "     - " .. funNameToHelpText[functName] .. "\n")
 end
 
+-- Cursor locator
+-- Adapted (slightly) from: https://gist.github.com/ttscoff/cce98a711b5476166792d5e6f1ac5907
+
+local mouseCircle = nil
+local mouseCircleTimer = nil
+
+function mouseHighlight()
+  size = 70
+    -- Delete an existing highlight if it exists
+    if mouseCircle then
+        mouseCircle:delete()
+--        mouseCircle2:delete()
+        if mouseCircleTimer then
+            mouseCircleTimer:stop()
+        end
+    end
+    -- Get the current co-ordinates of the mouse pointer
+    mousepoint = hs.mouse.getAbsolutePosition()
+    -- Prepare a big red circle around the mouse pointer
+    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-(size/2), mousepoint.y-(size/2), size, size))
+--    mouseCircle2 = hs.drawing.circle(hs.geometry.rect(mousepoint.x-(size/4), mousepoint.y-(size/4), size/2, size/2))
+    mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+--    mouseCircle2:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+    mouseCircle:setFill(false)
+--    mouseCircle2:setFill(false)
+    mouseCircle:setStrokeWidth(5)
+--    mouseCircle2:setStrokeWidth(5)
+    mouseCircle:show()
+--" "     mouseCircle2:show()
+
+    -- Set a timer to delete the circle after 3 seconds
+    mouseCircleTimer = hs.timer.doAfter(1, function() mouseCircle:delete() 
+    	--mouseCircle2:delete() 
+    end)
+end
+hs.hotkey.bind(HyperFn, "=", mouseHighlight)
+
 return miscFunctions
