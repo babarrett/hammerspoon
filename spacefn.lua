d20c5733217d888c2ff4f92b24567acbd738cd8a
Bruce Barrett <bruceb@earthreflections.com>
2017-09-27 13:56:06 -0700

    MAJOR: clean up and clear algorithm, delete most other user's notes.

--- spacefn.lua
+++ spacefn.lua
@@ -1,29 +1,39 @@
 local spacefn = {}
--- SpaceFn mappings
--- Keyboard mapping from Space+X to action. (Often CMD+key)
--- Modified to fit my preferred method of adding functions
-
+--  Two main functions, melded together:
+-- 1. SpaceFn mappings
+--  Keyboard mapping from Space+Key to action. For my current keuboards
+--    this includes: Navigation, Cut, Copy, Paste, Undo, Backspace, Del,
+--    PgUp, PgDn, Home, End, Load find selection, Find, Find next.
+--
+--  2. "Sticky Keys" for modifiers (⌘⌥⌃⇧)
+--    Use reportLayerModifierChange.lua to display mod changes in HUD
+--    TODO: Add support, after SpaceFn is complete
+--
 --  ⌘⌥⌃⇧     -- Visual representation
---  Start Simple
+--  Left hand Cut, Paste ans Find functions:
 --	SpaceFn+Z		Undo
 --	SpaceFn+X		Cut to Clipboard
 --	SpaceFn+C		Copy to Clipboard
 --	SpaceFn+V		Paste Clipboard
---  Navigation Cluster:
---                      ⌫ word   Delete
---    ↖️  ⬆️    ⇞                     U, I, O
+--	SpaceFn+D		Load Find selection
+--	SpaceFn+F   Find
+--	SpaceFn+G   Find next
+--  SpaceFn+E   Esc (used to exit out of mistakenly started SpaceFn)
+--  SpaceFn+B   Space (repeats)
+--
+--  Right hand Navigation Cluster:
+--   ⌫   ⬆️    Del                U, I, O
 --⬅⇧  ⬅️  ⬇️    ➡️  ⇧➡️              H, J, K, L, ;  (Outer arrows for select)
---        ↙️  ⬇️    ⇟                   M, ,, .
+--    ↖️    ↙️  (nop)   ⇞   ⇟            N, M, ,, .
 --
 --  Behaviors and operations:
 --    Unused keys: if you hold the space bar and press a key that has no
 --    function in the SpaceFn layout, you get exactly the same result as
 --    if you were not holding space.
-      --    There is an Esc key in the Fn layer (E key), because you may want to have direct
-      --    access to the backquote in the basic layout. Also, it may be convenient to be
-      --    able to press Esc without releasing the space bar.
---    SpaceFN+B for repeating spaces (because holding Space won't repeat any more)
---    NO: double tap the space bar (2 taps in X milliseconds) to trigger repeating
+      --    There is an Esc key in the SpaceFn "layer" (E key), because you may want to 
+      --    have direct access to the backquote in the basic layout. Also, it may be 
+      --    convenient to be able to press Esc without releasing the space bar.
+--    SpaceFn+B allows for repeating spaces (because holding Space won't repeat any more)
 --    TODO: Support SpaceFn+Mod+(Shift+Arrow) for select word and line.
 --    Approx. 300ms timeout on Space bar, so:
 --      Space, < 300ms, release = " "
@@ -32,80 +42,11 @@ local spacefn = {}
 --
 --Legend:
 --  Spc - space bar
---  k   - normal key (a-z,0-9, punctuation,etc.)
+--  k   - normal, visible, non-modifier key (a-z,0-9, punctuation, tab, etc.)
 --  mod - modifier key (Shift, Control, Alt/Option, Win/Command)
 --  --> - arrow key
+--  Esc - Escape
 --
---Normal operations:
---  Generate a space character (tap for space)
---  Spc   -----\_____/-----
---
---  Generate a SpaceFn character (unambigious case)
---  Spc   -----\____________/-----
---  k     --------\_____/-----
---
---  Cancel a mistakenly started SpaceFn function.
---  At least Esc will cancel. TBD: Will any non-SpaceFn key cancel? or be treated
---  as the un-SpaceFn character?
---  Spc   -----\____________/-----
---  Esc   --------\_____/---------
---
---
---
---Less common cases to be handled:
---  Space + Option + RightArrow + RightArrow + RightArrow moves right three words (OS X)
---  Spc   -----\_____________________________________________/-----
---  mod   --------\_______________________________________/-----
---  k     -----------\______/------\______/------\______/-----
---
---  Using Space+##s to cause repeat of the next character if it is: Any arrow, delete/backspace, del
---  Space + 1 + 1 + RightArrow moves right 11 characters.
---  Spc   -----\_____________________________________________/-----
---  1     -----------\______/------\______/------------------------
---  -->   --------------------------------------\______/-----------
---
---
---
---Ambigious, or edge-case conditions:
---  k starts during Spc down, but ends after Spc up.
---  ("Rollover issue" caused by starting k before fully releasing Spc. Fast typist.)
---  Interpret this as: Space, k
---  Spc   -----\__________/-----
---  k     --------------\______/-----
---
---    wilderjds: What I ended up doing, and it works quite well even in actual real world typing,
---    at least for me, is the following:
---      #1. If spacebar is pressed, then k is released, treat the spacebar press as a space press.
---      #2. If spacebar is pressed, then k is pressed and spacebar is released, treat the
---        spacebar events as space events
---    #3. Otherwise treat the spacebar press (and the following release) as a SpaceFn.
---    I guess the key thing is the first of the checks. It makes all the difference in
---    the world for me.
---
---    There are five possible relationships between Space and k:
---    * Space and k don't overlap. Unambiguous.
---    * k overlaps the start of Space. Treat as k, Space
---    * k overlaps the end of Space. Treat as Space, k
---    * k is wholly within Space. Treat as SpaceFn+k
---    * Space is wholly within k. Treat as SpaceFn+k
---  Spc   -----\____/----------------
---  k     --------------\______/-----
---
---  Spc   -------\__________/--------
---  k     ----\______/---------------
---        Rule #1 says this is: k, Space
---
---  Spc   -----\__________/-----
---  k     --------------\______/-----
---        Rule #2 says this is: Space, k
---
---  Spc   -----\___________________/-----
---  k     --------------\______/-----
---        Rule #3 says this is: SpaceFn+k
---
---  Spc   ------------\____/---------
---  k     ------\______________/-----
---        Rule #3 says this is: SpaceFn+k
 --
 --  ----------------------------------------------------------------------
 --  --------------------- Alternate thoughts, Bruce ----------------------
@@ -136,6 +77,13 @@ local spacefn = {}
 --  k     --------------\______/------------
 --        Spc Dn, k Dn, k Up, Spc Up = Swallow Sp, Emit Spc+k
 --
+---- #6: k down, Spc down, k up, Spc up after k
+--  Spc   -------\__________/---------------                !spcDnStart
+--  k     ----\______/----------------------
+--            k             Sp
+--        k Dn, Emit, Spc Dn, (don't emit), k Up, Emit k Up
+--        Spc Up = Emit Spc Dn/Up (no Spc+k's emited to the Spc Dn/Up emits a Spc)
+--
 ---- #7: k1 down, Spc down, k1 up, k2 Dn, k2 Up, Spc up
 --  Spc   -------\____________________/-----                !spcDnStart
 --  k     ----\______/----\______/----------
@@ -144,122 +92,30 @@ local spacefn = {}
 --        k2 Dn (emit Spc+k2 Dn), k2 Up (emit Spc+k2 Up)
 --        Spc Up = Emit Spc Dn/Up (no Spc+k's emited to the Spc Dn/Up emits a Spc)
 --
+----  #8: LATER
+--  Spc   ------------\____/----------------                !spcDnStart
+--  k     ------\______________/------------
+--        This could be: k Down, Space Down, Space Up, k Up = k, Spc
+--        Uncommon enough to be undefined?
+--
 --  So, what's the algorithm for this?
 --
---  Start:      "At rest" state. Any key typed will behave "normally"
---              Set spcDnStart, spcKEmitted, tossSpcUp, tossEscUp = false
---              keysToRelease = {}
---              keysDownInSpc = {}
+--  Start:     
 --
---  k Dn:       if ((k == Esc) and (spcDnStart)) then
---                tossSpcUp = true
---                tossEscUp = true
---                push k to keysToRelease
---              else
---                If !spcDnStart then
---                  emit k Dn
---                  exit;
---                If spcDnStart then
---                  -=- what next?
---  push k to keysDownInSpc; "Swallow" k dn
+--  k Dn:      
 --
+--  k Up:      
 --
---              push k to keysToRelease
+--  Spc Dn:    
 --
---              If !spcDnStart then emit k Dn;
---
---  k Up:       remove k from keysToRelease
---              If spcDnStart then emit k Up; Set flag kTypedinSpc = true;
---                exit
---              if k is in keysDownInSpc then remove k from keysDownInSpc; Spc+k;     -- was: emit k dn, k up
---              else
---                  If !spcDnStart then emit k Up;
---
---  Spc Dn:     Set spcDnStart = true
---              /// Set startTimeMs = now in ms (needed?)
---              "Swallow" Spc Dn
---
---  Spc Up:     spcDnStart = false
+--  Spc Up:    
 --
 --  CleanEnd:
 --
 --
 --  spcDnStart  kTypedinSpc keysToRelease keysDownInSpc EMIT
 --  false       false       {}          {}            ""    --start
--1- false       false       {}          {}            ""    --start
---  true
---  false                                             Spc dn, Spc up
---                          {k}                       k dn
---                          {}                        k up
---
--2- false       false       {}          {}            ""    --start
---                          {k}                       k dn
---  true                                              .
---              true        {}          {k}           k up
---              false
---  false
---
---
---  Hasu says: (DR = Duel Role)
---    Rules determining whether DRmod should perform as key or modifier are very simple:
---    1. Judging term (200ms in my case) elapsed(timeout) => modifier
---    3. DRmod is released within 200ms(tapping)  => key
---    Need to buffer keys while Space is down.
---
---    Placing modifiers on home row is nice idea. I've used ';' as modifier happily
---    for a few years but never tried other home keys. If you use alpha keys as
---    modifier it'll be important to choke off false detection as possible with
---    tweaking code or parameters. It is really challenging idea.
---  Lydell
---    One thing that really made a big difference in my script, is the ability to
---    have key-level settings (which I added in version 0.3.0). I use 70ms delay by
---    default, and 200ms for the alt and Windows dual-role keys.
 
---    Why is it necessary to have a delay?
---    Think of a dual-role key is comprised of Space and Shift.
---    When you type, for example, "a man" fast you will/may press 'm' before
---    releasing space key. Without the delay you will get "aMan" in this case.
-
---    More research (paper): http://edgarmatias.com/papers/hci96
---    BTW, while one-handed typing is very cool, I found that replacing Caps Lock with
---    an Fn key (and a well designed Fn layer) is a lot more useful in day-to-day use...
---    http://matias.ca/optimizer
---    http://matias.ca/optimizer/viewer/?p=5
---    The Backspace, Enter, Copy/Paste shortcuts on the left hand are expecially handy.
-
---    My use of dual role keys: ***
---    1. space as shift: we usually don't press space and shift at the same time, this
---    setting helps to keep my hands on the home row.
---    2. "f" used to activate navigation layer: I have a layer in which the right hand
---    home row keys are assigned to navigation keys, since we seldom use characters
---    and navigation keys (arrow keys) in combination, this setting allows me to keep
---    my hands on the home row all the time.
-
---    Checking release order can prevent most errors as well. The potential
---    downside is that you may have to retrain yourself to always release modifier
---    keys last (though this isn't a huge deal). A combination of this and a delay may
---    be very reliable.
-
---  Possible interactions
-
-
-
---  How to do this...
---  hs.eventtap and hs.eventtap.event
---  hs.eventtap.new(types, fn) -> eventtap
---      types = {"keyDown", "keyUp"}
---      fn = mycallbackfunction when event occures
---
---  SpaceFnDown = false
---  Tap Events, set callback for {"keyDown", "keyUp"}
---
---  callback(event)
---    if key == "space" and type == "keyDown"
---      SpaceFnDown = true
---    if key == "space" and type == "keyUp"
---      SpaceFnDown = false
---      .
---
 --
 --
 --
@@ -340,16 +196,3 @@ end
 
 
 return spacefn
----- #6: k down, Spc down, k up, Spc up after k
---  Spc   -------\__________/---------------                !spcDnStart
---  k     ----\______/----------------------
---            k             Sp
---        k Dn, Emit, Spc Dn, (don't emit), k Up, Emit k Up
---        Spc Up = Emit Spc Dn/Up (no Spc+k's emited to the Spc Dn/Up emits a Spc)
---
-----  #8: LATER
---  Spc   ------------\____/----------------                !spcDnStart
---  k     ------\______________/------------
---        This could be: k Down, Space Down, Space Up, k Up = k, Spc
---        Uncommon enough to be undefined?
---
