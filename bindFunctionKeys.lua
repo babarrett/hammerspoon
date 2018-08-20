b022d8785cacd234f370cd3ce4ca2b0dc7ca33f3
BruceBarrett <bruceb@earthreflections.com>
2018-08-20 11:17:30 -0700

    Moe functions off F1-F12 to Shift+F## so I can use straight F#s for IDE and FMP development.

--- bindFunctionKeys.lua
+++ bindFunctionKeys.lua
@@ -1,4 +1,4 @@
--- Bind Function Keys (F1-F24) to one of 3 functions:
+-- Bind Function Keys (Shift+F1-8, F13-F24) to one of 3 functions:
 -- 	* Launch (or select) App
 --	* Open web page
 --	* Type a key (and optional modifier)
@@ -35,22 +35,20 @@ end
 --   Key modifier if any, Key to bind, Display message  (nil, not used),
 --   App to launch or bundleID, Web site to open, Keypress to simulate (Example: Cmd+shift+".")
 
-hs.hotkey.bind("", "f1", nil, function() launchAppOrWeb( 'BBEdit', nil, nil) end )
-hs.hotkey.bind("", "f2", nil, function() launchAppOrWeb( 'iTerm', nil, nil) end )
-hs.hotkey.bind("", "f3", nil, function() launchAppOrWeb( 'Safari', nil) end )
-hs.hotkey.bind("", "f4", nil, function() launchAppOrWeb( 'Finder', nil) end )
-hs.hotkey.bind("", "f5", nil, function() launchAppOrWeb( 'Mail', nil) end )
-hs.hotkey.bind("", "f6", nil, function() launchAppOrWeb( nil, nil, {mods='CMD', char="1"} ) end )		-- CMD+1 = View Mail inbox
-hs.hotkey.bind("", "f7", nil, function() launchAppOrWeb( nil, nil, {mods='CMD Shift', char="."}) end )	-- Cmd+Shift+"." = for move email to Done folder
-hs.hotkey.bind("", "f8", nil, function() launchAppOrWeb( nil, nil, {mods='CMD Shift', char=","}) end )	-- Cmd+Shift+"," = for move email to Status folder
+hs.hotkey.bind("Shift", "f1", nil, function() launchAppOrWeb( 'BBEdit', nil, nil) end )
+hs.hotkey.bind("Shift", "f2", nil, function() launchAppOrWeb( 'iTerm', nil, nil) end )
+hs.hotkey.bind("Shift", "f3", nil, function() launchAppOrWeb( 'Safari', nil) end )
+hs.hotkey.bind("Shift", "f4", nil, function() launchAppOrWeb( 'Finder', nil) end )
+hs.hotkey.bind("Shift", "f5", nil, function() launchAppOrWeb( 'Mail', nil) end )
+hs.hotkey.bind("Shift", "f6", nil, function() launchAppOrWeb( nil, nil, {mods='CMD', char="1"} ) end )		-- CMD+1 = View Mail inbox
+--hs.hotkey.bind("Shift", "f7", nil, function() launchAppOrWeb( nil, nil, {mods='CMD Shift', char="."}) end )	-- Cmd+Shift+"." = for move email to Done folder
+--hs.hotkey.bind("Shift", "f8", nil, function() launchAppOrWeb( nil, nil, {mods='CMD Shift', char=","}) end )	-- Cmd+Shift+"," = for move email to Status folder
 
--- No idea why, but these 3 F## fail. Replaced with Karabiner.
---hs.hotkey.bind("", "f9", nil, function() launchAppOrWeb( 'Preview', nil, nil) end )
---hs.hotkey.bind("", "f10", nil, function() launchAppOrWeb( "/Users/bbarrett/Secure.dmg & open /Users/bruce/Secure.dmg", nil, nil) end )	-- open Secure.dmg either at work or at home.
---hs.hotkey.bind("", "f11", nil, function() launchAppOrWeb( 'System Preferences', nil) end )
-
---No longer using Oxygen, disabled. Key is free for other uses. Nov 2017
---hs.hotkey.bind("", "f12", nil, function() launchAppOrWeb( 'Oxygen XML Author', nil) end )
+-- No idea why, but these (F9 to F11, and Shift-F10 to Shift-F11 fail. (macOS intercepts them?) Replaced with Karabiner.
+hs.hotkey.bind("Shift", "f9", nil, function() hs.eventtap.keyStroke({"cmd", "shift"}, "[") end )  -- Prev browser tab
+--hs.hotkey.bind("Shift", "f10", nil, function() hs.alert( 's-f10' ) end )
+--hs.hotkey.bind("Shift", "f11", nil, function() hs.alert( 's-f11' ) end )
+hs.hotkey.bind("Shift", "f12", nil, function() hs.eventtap.keyStroke({"cmd", "shift"}, "]") end )  -- Next browser tab
 
 -- www short cuts
 -- F13..20(24) are for the GH-122 keyboard
@@ -62,21 +60,8 @@ hs.hotkey.bind("", "f8", nil, function() launchAppOrWeb( nil, nil, {mods='CMD Sh
 --hs.hotkey.bind("Shift", "f2", nil, function() launchAppOrWeb( nil, "https://drive.google.com/drive/my-drive") end )
 --hs.hotkey.bind("", "f15", nil, function() launchAppOrWeb( nil, "https://docs.google.com/document/u/0/?tgif=c") end )
 --hs.hotkey.bind("Shift", "f3", nil, function() launchAppOrWeb( nil, "https://docs.google.com/document/u/0/?tgif=c") end )
-hs.hotkey.bind("", "f16", nil, function() launchAppOrWeb( nil, "https://sheets.google.com") end )
-hs.hotkey.bind("Shift", "f4", nil, function() launchAppOrWeb( nil, "https://sheets.google.com") end )
-
-hs.hotkey.bind("", "f17", nil, function() launchAppOrWeb( nil, "https://jira.aspera.us/projects/ASCN?selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=no-filter") end )
-hs.hotkey.bind("Shift", "f5", nil, function() launchAppOrWeb( nil, "https://jira.aspera.us/projects/ASCN?selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page&status=no-filter") end )
-hs.hotkey.bind("", "f18", nil, function() launchAppOrWeb( nil, "https://confluence.aspera.us/display/CON/Connect+Browser+Plug-in+Home") end )
-hs.hotkey.bind("Shift", "f6", nil, function() launchAppOrWeb( nil, "https://confluence.aspera.us/display/CON/Connect+Browser+Plug-in+Home") end )
-hs.hotkey.bind("", "f19", nil, function() launchAppOrWeb( nil, "https://trac.aspera.us") end )
-hs.hotkey.bind("Shift", "f7", nil, function() launchAppOrWeb( nil, "https://trac.aspera.us") end )
-hs.hotkey.bind("", "f20", nil, function() launchAppOrWeb( nil, "https://confluence.aspera.us/display/TP/Technical+Publications") end )
-hs.hotkey.bind("Shift", "f8", nil, function() launchAppOrWeb( nil, "https://confluence.aspera.us/display/TP/Technical+Publications") end )
-
---    F21, nil, "https://developer.asperasoft.com"},
---    F22, nil, "https://aspera.zendesk.com/agent/dashboard"},
---    F23, nil, "http://downloads.asperasoft.com"},
+--hs.hotkey.bind("", "f16", nil, function() launchAppOrWeb( nil, "https://sheets.google.com") end )
+--hs.hotkey.bind("Shift", "f4", nil, function() launchAppOrWeb( nil, "https://sheets.google.com") end )
 
 hs.hotkey.bind("Cmd Shift", "f10", nil, function() 	hs.eventtap.keyStroke({""}, "f10") end )
 
